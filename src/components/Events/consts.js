7495ee09dd78c65ad7d27f8ca068bdf8228bbe31
duyhiep2519 <duyhiep2519@gmail.com>
2023-01-09 17:32:45 +0700

    add grid in events

--- src/components/Events/consts.js
+++ src/components/Events/consts.js
@@ -1,9 +1,10 @@
 import ngocmai from "assets/images/ngocmai.jpg";
+import hieptd from "assets/images/hieptd.jpg";
 
 export const Births = {
   boy: {
     day: "1999-01-25",
-    avatar: "",
+    avatar: hieptd,
     name: "Duy Hiệp",
   },
   girl: {
--- src/components/Events/index.js
+++ src/components/Events/index.js
@@ -1,11 +1,18 @@
 import { BirthDayCard } from "./BirthdayCard";
 import { Births } from "./consts";
+import { Col, Row } from "antd";
 
 export const Events = () => {
   return (
     <>
-      <BirthDayCard birthday={Births.girl} />
-      <BirthDayCard birthday={Births.boy} />
+      <Row className="gutter-row" gutter={16}>
+        <Col span={12}>
+          <BirthDayCard birthday={Births.girl} />
+        </Col>
+        <Col span={12}>
+          <BirthDayCard birthday={Births.boy} />
+        </Col>
+      </Row>
     </>
   );
 };
